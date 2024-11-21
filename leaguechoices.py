851c1b19725251108694bc612060f67d455e7796
Tyler Nex <tylernex@gmail.com>
2024-11-20 18:29:31 -0800

    now generates combat mastery graphics

--- leaguechoices.py
+++ leaguechoices.py
@@ -1,4 +1,8 @@
 import random
+import numpy
+import imageio
+import os
+from PIL import Image
 
 REGIONS = ['Morytania', 'Varlamore', 'Fremmenik', 'Kandarin',
            'Desert', 'Wilderness', 'Tirannwn', 'Kourend', 'Asgarnia']
@@ -40,16 +44,55 @@ class LeagueChoices:
             if self.combat_masteries[style] < 6:
                 self.combat_masteries[style] += 1
             else:
-                self.combat_masteries[(style + 1) % 3] += 1
-  
-def generate_rand_league():
-    a = LeagueChoices()
-    a.randomize()
+                self.combat_masteries[(style + 1 - 2 * random.randint(0,1)) % 3] += 1
+                
+    def create_combat_graphic(self) -> Image.Image:
+        graphic = Image.open('./images/base.png')
+        melee_images = ['./images/melee1.png', './images/melee2.png', './images/melee3.png', 
+                        './images/melee4.png', './images/melee5.png', './images/melee6.png']
+        range_images = ['./images/range1.png', './images/range2.png', './images/range3.png', 
+                        './images/range4.png', './images/range5.png', './images/range6.png']
+        mage_images = ['./images/mage1.png', './images/mage2.png', './images/mage3.png', 
+                       './images/mage4.png', './images/mage5.png', './images/mage6.png']
+        tier_images = ['./images/tier1.png', './images/tier2.png', './images/tier3.png', 
+                       './images/tier4.png', './images/tier5.png', './images/tier6.png']
+        passive_images = ['./images/passive1.png', './images/passive2.png', './images/passive3.png', 
+                          './images/passive4.png', './images/passive5.png', './images/passive6.png']
+        tier_width = 240
+        x_offset = 480
+        melee_y_offset = 40
+        range_y_offset = 305
+        mage_y_offset = 588
+        passive_y_offset = 872
+        highest_tier = max(self.combat_masteries[0], max(self.combat_masteries[1], self.combat_masteries[2]))
+        for i in range(highest_tier):
+            tier = Image.open(tier_images[i])
+            graphic.paste(tier, (x_offset + tier_width * i, 0), mask=tier)
+            passive = Image.open(passive_images[i])
+            graphic.paste(passive, (x_offset + tier_width * i, passive_y_offset), mask=passive)
+        for i in range(self.combat_masteries[0]):
+            img = Image.open(melee_images[i])
+            graphic.paste(img, (x_offset + tier_width * i, melee_y_offset), mask=img)
+        for i in range(self.combat_masteries[1]):
+            img = Image.open(range_images[i])
+            graphic.paste(img, (x_offset + tier_width * i, range_y_offset), mask=img)
+        for i in range(self.combat_masteries[2]):
+            img = Image.open(mage_images[i])
+            graphic.paste(img, (x_offset + tier_width * i, mage_y_offset), mask=img)
+        
+        return graphic
+ 
+def generate_rand_league() -> LeagueChoices:
+    build = LeagueChoices()
+    build.randomize()
+    return build
+   
+if __name__ == '__main__':
+    random.seed()
+    a = generate_rand_league()
     print('Randomized League Choices:')
     print(f'  Regions: {', '.join(a.regions)}\n'
          +f'  Combat Masteries: Melee {a.combat_masteries[0]}, Ranged {a.combat_masteries[1]}, Mage {a.combat_masteries[2]}\n'
          +f'  Relics: {', '.join(a.relics)}')
-
-if __name__ == '__main__':
-    random.seed()
-    generate_rand_league()
+    a.combat_masteries = [5,5,0]
+    a.create_combat_graphic().save('combat-graphic-generated.png')
